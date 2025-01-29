93ee9737f5a17b9fa5d78869c10e32c3cfaff3dc
Abrham Belayineh <118188730+abrsh6266@users.noreply.github.com>
2025-01-29 05:13:08 -0800

    get phot method created

--- src/main/java/com/spring/abrsh/photoz_clone/PhotozController.java
+++ src/main/java/com/spring/abrsh/photoz_clone/PhotozController.java
@@ -1,14 +1,23 @@
 package com.spring.abrsh.photoz_clone;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PhotozController {
 
+    private List<Photo> db = List.of(new Photo("1", "photo1.jpg"), new Photo("2", "photo2.jpg"));
+
     @GetMapping("/")
     public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/photos")
+    public List<Photo> get() {
+        return db;
     }
 
 }
