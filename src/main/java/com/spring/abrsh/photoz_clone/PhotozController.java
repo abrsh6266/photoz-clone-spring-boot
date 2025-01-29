efbd04f166e464dfe8f1bf857080c7f2c0c1017a
Abrham Belayineh <118188730+abrsh6266@users.noreply.github.com>
2025-01-29 06:00:53 -0800

    delete and post apis added

--- src/main/java/com/spring/abrsh/photoz_clone/PhotozController.java
+++ src/main/java/com/spring/abrsh/photoz_clone/PhotozController.java
@@ -1,14 +1,28 @@
 package com.spring.abrsh.photoz_clone;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class PhotozController {
-
-    private List<Photo> db = List.of(new Photo("1", "photo1.jpg"), new Photo("2", "photo2.jpg"));
+    private Map<String, Photo> db = new HashMap<>() {
+        {
+            put("1", new Photo("1", "photo1.jpg"));
+            put("2", new Photo("2", "photo2.jpg"));
+        }
+    };
 
     @GetMapping("/")
     public String hello() {
@@ -16,8 +30,30 @@ public class PhotozController {
     }
 
     @GetMapping("/photos")
-    public List<Photo> get() {
-        return db;
+    public Collection<Photo> get() {
+        return db.values();
     }
 
+    @GetMapping("/photos/{id}")
+    public Photo getOne(@PathVariable String id) {
+        Photo photo = db.get(id);
+        if (photo == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Photo not found");
+        }
+        return db.get(id);
+    }
+
+    @DeleteMapping("/photos/{id}")
+    public void delete(@PathVariable String id) {
+        if (db.remove(id) == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Photo not found");
+        }
+    }
+
+    @PostMapping("/photos")
+    public Photo create(@RequestBody Photo photo) {
+        photo.setId(UUID.randomUUID().toString());
+        db.put(photo.getId(), photo);
+        return photo;
+    }
 }
